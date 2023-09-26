90f19b1b6c47ff5e8fb4775e4885ec84d4ea73bd
Marc <marcsans26@gmail.com>
2023-09-26 18:13:41 +0200

    exercisis repas

---
+++ src/Exercisis.java
@@ -0,0 +1,30 @@
+public class Exercisis {
+    //Exercici 1
+    //A aquest programa hem de definir dos nombre enters amb el valor que vulgueu
+    //vosaltres. Per pantalla hem de mostrar la suma, la resta la multiplicació i la divisió
+    //d'aquests dos nombres. (Una línia per cada resultat.)
+
+
+    //Exercici 2
+    //A aquesta activitat hem de dissenyar un programa que et faci la mitjana de les notes
+    //d'un alumne, per a això el programa t'ha de sol·licitar el nom de l'alumne i les notes
+    //de les 3 avaluacions per pantalla i mostrar el resultat final.
+
+
+    //Exercici 3
+    //A aquesta activitat hem de dissenyar un programa que calculi el sou d'un
+    //treballador. Ha de demanar quantes hores fa feina al dia, quants de dies al mes fa
+    //feina i a quant cobra les hores. Finalment ha de mostrar el resultat per pantalla.
+
+
+    //Exercici 4
+    //A aquesta activitat hem de dissenyar un programa que sigui capaç de llegir per
+    //teclat mitjançant "InputStreamReader" i "BufferedReader" diferents tipus de dades
+    //(String, Int, float i double) tenint en compte diferents excepcions utilitzant l'estructura
+    //try i catch.
+
+
+    //Exercici 5 A aquesta activitat hem de dissenyar un programa amb dos mètodes. Un
+    //mètode "llegir" haurà d'imprimir per pantalla el contingut per pantalla d'un fitxer local.
+
+}
