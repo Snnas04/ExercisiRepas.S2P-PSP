98745ff3b5c6d23c3acbbc2457615c516d924a80
Marc <marcsans26@gmail.com>
2023-09-26 18:30:27 +0200

    Exercisi 1

--- src/Exercisis.java
+++ src/Exercisis.java
@@ -1,11 +1,22 @@
 public class Exercisis {
+    public static void main(String[] args) {
+        Calculadora(4, 2);
+    }
+
     //Exercici 1
     //A aquest programa hem de definir dos nombre enters amb el valor que vulgueu
-    //vosaltres. Per pantalla hem de mostrar la suma, la resta la multiplicació i la divisió
+    //vosaltres. Per pantalla hem de mostrar la suma, la resta, la multiplicació i la divisió
     //d'aquests dos nombres. (Una línia per cada resultat.)
 
-    public static void main(String[] args) {
+    private static void Calculadora(int num1, int num2) {
+        int resultSum, resultRes, resultMul, resultDiv;
 
+        resultSum = num1 + num2;
+        resultRes = num1 - num2;
+        resultMul = num1 * num2;
+        resultDiv = num1 / num2;
+
+        System.out.println("Suma: " + resultSum + "\nResta: " + resultRes + "\nMultiplicació: " + resultMul + "\nDivició: " + resultDiv);
     }
 
     //Exercici 2
